9680ff1757cbcb6fb4e9405108841d447627197d
Henrique Bernardes <henriquepabe@gmail.com>
2021-11-27 05:13:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const upLeft = document.getElementById("up-left")
+const upRight = document.getElementById("upRight")
+const downLeft = document.getElementById("downLeft")
+const downRight = document.getElementById("downRight")
+
+function envia(){
+    console.log(upLeft.value)
+    document.getElementById("lemon").style.borderRadius = "30%"
+
+}
