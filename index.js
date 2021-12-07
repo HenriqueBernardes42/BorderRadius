7cb87cb5538e002df6c5345405efff666318f5ba
Henrique p. Bernardes <henriquepabe@gmail.com>
2021-12-06 22:32:47 -0300

    projeto pronto para ser postado

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const upLeft = document.getElementById("up-left")
-const upRight = document.getElementById("upRight")
-const downLeft = document.getElementById("downLeft")
-const downRight = document.getElementById("downRight")
+const upRight = document.getElementById("up-right")
+const downLeft = document.getElementById("down-left")
+const downRight = document.getElementById("down-right")
 
 function envia(){
-    console.log(upLeft.value)
-    document.getElementById("lemon").style.borderRadius = "30%"
+    document.getElementById("lemon").style.borderRadius = 
+    `${upLeft.value}% ${upRight.value}% ${downLeft.value}% ${downRight.value}%`    
 
+    document.getElementById("board").innerHTML =
+     `border radius :${upLeft.value}px ${upRight.value}px ${downLeft.value}px ${downRight.value}px` 
 }
+
+
+
+
+ 
